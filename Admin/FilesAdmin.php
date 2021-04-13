2d37589c2bea6c05a03bddf697bf3a040382e79f
gekomod <zaba141@o2.pl>
2021-04-13 02:50:01 +0200

    Update FilesAdmin.php

--- Admin/FilesAdmin.php
+++ Admin/FilesAdmin.php
@@ -3,7 +3,7 @@
 namespace Gekomod\FilesBundle\Admin;
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
-use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Route\RouteCollectionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class FilesAdmin extends AbstractAdmin
@@ -11,7 +11,7 @@ final class FilesAdmin extends AbstractAdmin
     protected $baseRoutePattern = 'files';
     protected $baseRouteName = 'files';
 
-    protected function configureRoutes(RouteCollection $collection)
+    protected function configureRoutes(RouteCollectionInterface $collection): void
     {
         $collection->clearExcept(['list']);
         $collection->add('filesfolders', 'filesfolders/{dirs}');
